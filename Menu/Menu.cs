d57a10ea18419da4d0f2af4ae7503d4d477766b7
Aaron <140290205+adostacey@users.noreply.github.com>
2025-07-23 10:37:25 -0500

    Init: menu classes

---
+++ Menu/Menu.cs
@@ -0,0 +1,41 @@
+namespace DiceRoller.Menu.Menu;
+
+public class Menu
+{
+    private readonly List<MenuItem> _menuItems;
+}
+
+
+public abstract class MenuItem
+{
+    public string Title { get; private set; }
+
+    protected MenuItem(string title)
+    {
+        Title = title;
+    }
+
+    protected abstract void Open();
+}
+
+
+public class SubMenuItem : MenuItem
+{
+    private readonly List<MenuItem> _subItems;
+
+    protected SubMenuItem(string title, List<MenuItem> subItems) : base(title)
+    {
+        _subItems = subItems;
+    }
+
+    protected override void Open() { }
+}
+
+public class ExecMenuItem : MenuItem
+{
+    private required readonly Action _action; // delegate 
+
+    protected ExecMenuItem(string title) : base(title) { }
+
+    protected override void Open() => _action();
+}
